1f707c333e63ce7fbdf4ab421c6ddc2fb3adb437
unknown <jeremyxu1998@gmail.com>
2018-07-22 11:53:16 -0400

    clean up and add README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Reversi_Chess_Game
-This is a Reversi game written in C for APS105 course project.
+This is a Reversi game written in C for APS105 course project. It can print a chessboard on command line, and has a computer playing with user.
+
+The computer's logic of deciding where to put the next chess: 
+1. How many opponent's pieces can be flipped on current move. 
+2. Get the corner or side, and avoid position next to corner or side. 
+3. Avoid move that can be flipped in the next move of opponent. 
+I've also tried "predicting" for a few steps (i.e. let computer play with itself to decide if this is a good move), but the result wasn't ideal.
+
+With these three simple logics, the program is powerful enough to beat assignment's test, both easy and hard level.
--- Reversi.c
+++ Reversi.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdbool.h>
-//#include "lab7part2lib.h"
+// #include "lab7part2lib.h"  // Testing library
 
 void initialBoard(char board[][26], int n);
 void printBoard(char board[][26], int n);
@@ -21,9 +21,9 @@ void changeColour(char board[][26], int n, char row, char col,
     char colour, char opcolour, int deltaRow, int deltaCol);
 void getWinner(char board[][26], int n);
 
-/*
-* In part two, computer is designed as the algorism indicated below
-*/
+
+// In part two, computer is designed as the algorism indicated below
+
 int main() {
     char board[26][26];
     int n;
@@ -33,28 +33,36 @@ int main() {
     scanf("%d", &n);
     printf("Computer plays (B/W) : ");
     scanf(" %c", &sideComputer);
+
     initialBoard(board, n);
     bool compHasMove = true;
     bool playerHasMove = true;
     bool playerMistake = false;
+
     while (compHasMove == true || playerHasMove == true) {
-        if (currentMove == sideComputer) currentMove = computerMove(board, n, currentMove, &compHasMove);
+        if (currentMove == sideComputer)
+            currentMove = computerMove(board, n, currentMove, &compHasMove);
         else {
             currentMove = playerMove(board, n, currentMove, &playerHasMove, &playerMistake);
-            if (playerMistake) break;
+            if (playerMistake) break;  // In the rule, player making a mistake straightly lose the game
         }
     }
-    if (playerMistake) printf("%c player wins.\n", sideComputer);
-    else getWinner(board, n);
+    if (playerMistake)
+        printf("%c player wins.\n", sideComputer);
+    else
+        getWinner(board, n);
     return 0;
 }
 
 void initialBoard(char board[][26], int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (i == n / 2 - 1 && j == n / 2 - 1 || i == n / 2 && j == n / 2) board[i][j] = 'W';
-            else if (i == n / 2 && j == n / 2 - 1 || i == n / 2 - 1 && j == n / 2) board[i][j] = 'B';
-            else board[i][j] = 'U';
+            if ((i == n / 2 - 1 && j == n / 2 - 1) || (i == n / 2 && j == n / 2))
+                board[i][j] = 'W';
+            else if ((i == n / 2 && j == n / 2 - 1) || (i == n / 2 - 1 && j == n / 2))
+                board[i][j] = 'B';
+            else
+                board[i][j] = 'U';
         }
     }
     printBoard(board, n);
@@ -120,23 +128,22 @@ bool bestMoveComp(char board[][26], int n, char colour, char opcolour, char *bes
                         if (legal) {
                             wholeLegal = true;
                             positionLegal = true;
-                            //similar to part 1, calculate how many to flip in each direction and add them
+                            // Similar to part 1, calculate how many to flip in each direction and add them
                             score = scoreInOneDirection(board, n, row, col, colour, opcolour, deltaRow, deltaCol, score);
                         }
                     }
                 }
                 if (positionLegal) {
-                    //second part of changing score, try to get side and corner 
+                    // Second part of changing score, try to get side and corner 
                     if (row == 'a' || row == 'a' + n - 1 || col == 'a' || col == 'a' + n - 1) score = score + 2;
-                    if (row == 'a' && col == 'a' || row == 'a' + n - 1 && col == 'a' || row == 'a' && col == 'a' + n - 1 || row == 'a' + n - 1 && col == 'a' + n - 1)
+                    if ((row == 'a' && col == 'a') || (row == 'a' + n - 1 && col == 'a') || (row == 'a' && col == 'a' + n - 1) || (row == 'a' + n - 1 && col == 'a' + n - 1))
                         score = score + 3;
-                    //third part of changing score, try to avoid the row and column next to corner 
+                    // Third part of changing score, try to avoid the row and column next to corner 
                     if (n >= 6) {
-                        //if (row == 'a' + 1 || row == 'a' + n - 2 || col == 'a' + 1 || col == 'a' + n - 2) score = score - 1;
-                        if (row == 'a' + 1 && col == 'a' + 1 || row == 'a' + n - 2 && col == 'a' + 1 || row == 'a' + 1 && col == 'a' + n - 2 || row == 'a' + n - 2 && col == 'a' + n - 2)
+                        if ((row == 'a' + 1 && col == 'a' + 1) || (row == 'a' + n - 2 && col == 'a' + 1) || (row == 'a' + 1 && col == 'a' + n - 2) || (row == 'a' + n - 2 && col == 'a' + n - 2))
                             score = score - 2;
                     }
-                    //if this position can be immediately flipped by player next, it will have a lower mark
+                    // If this position can be immediately flipped by player next, it will have a lower mark
                     bool canBeFlipedNext = false;
                     canBeFlipedNext = flipNextOrNot(board, n, row, col, colour, opcolour);
                     if (canBeFlipedNext) {
@@ -144,8 +151,8 @@ bool bestMoveComp(char board[][26], int n, char colour, char opcolour, char *bes
                         else score = score - 2;
                     }
                 }
-                //printf("Score of %c%c is %d\n", row, col, score);
-                //if this position has a higher score, change best to this one
+                // printf("Score of %c%c is %d\n", row, col, score);
+                // If this position has a higher score, change best to this one
                 if (score > highestScore) {
                     highestScore = score;
                     *bestPosition = row;
@@ -165,20 +172,25 @@ char playerMove(char board[][26], int n, char currentMove, bool *playerHasMove, 
     wholeLegal = availableMoves(board, n, currentMove, opMove);
     if (wholeLegal) {
         *playerHasMove = true;
-        //int row;
-        //int col;
-        //findSmartestMove(board, n, currentMove, &row, &col);
-        //printf("Testing AI move (row, col): %c%c\n", row + 'a', col + 'a');
+        // For testing
+        /*
+        * int row;
+        * int col;
+        * findSmartestMove(board, n, currentMove, &row, &col);
+        * printf("Testing AI move (row, col): %c%c\n", row + 'a', col + 'a');
+        */
         printf("Enter move for colour %c (RowCol): ", currentMove);
         char move[3];
         move[0] = currentMove;
         for (int i = 1; i <= 2; i++) {
             scanf(" %c", &move[i]);
         }
-        //char move[3];
-        //move[0] = currentMove;
-        //move[1] = row+'a';
-        //move[2] = col+'a';
+        /*
+        * char move[3];
+        * move[0] = currentMove;
+        * move[1] = row+'a';
+        * move[2] = col+'a';
+        */
         doTheMove(board, n, move, playerMistake);
     }
     else {
@@ -219,16 +231,19 @@ bool checkLegalInDirection(char board[][26], int n, char row, char col,
         row = row + deltaRow;
         col = col + deltaCol;
         bool inBounds = positionInBounds(board, n, row, col);
-        if (board[(int)(row - 'a')][(int)(col - 'a')] == colour && board[(int)(row - 'a') - deltaRow][(int)(col - 'a') - deltaCol] == opcolour) legal = true;
-        else if (board[(int)(row - 'a')][(int)(col - 'a')] == colour && board[(int)(row - 'a') - deltaRow][(int)(col - 'a') - deltaCol] != opcolour
-            || board[(int)(row - 'a')][(int)(col - 'a')] == 'U' || !inBounds) break;
+        if (board[(int)(row - 'a')][(int)(col - 'a')] == colour && board[(int)(row - 'a') - deltaRow][(int)(col - 'a') - deltaCol] == opcolour)
+            legal = true;
+        else if ((board[(int)(row - 'a')][(int)(col - 'a')] == colour && board[(int)(row - 'a') - deltaRow][(int)(col - 'a') - deltaCol] != opcolour)
+            || (board[(int)(row - 'a')][(int)(col - 'a')] == 'U' || !inBounds))
+                break;
     }
     return legal;
 }
 
 bool positionInBounds(char board[][26], int n, char row, char col) {
     bool inBounds;
-    if (row >= 'a' && row <= 'a' + n - 1 && col >= 'a' && col <= 'a' + n - 1) inBounds = true;
+    if (row >= 'a' && row <= 'a' + n - 1 && col >= 'a' && col <= 'a' + n - 1)
+        inBounds = true;
     else inBounds = false;
     return inBounds;
 }
@@ -246,7 +261,7 @@ int scoreInOneDirection(char board[][26], int n, char row, char col,
     return score;
 }
 
-//function to decide whether this position will be flipped or not in next move by player
+// Function to decide whether this position will be flipped or not in next move by player
 bool flipNextOrNot(char board[][26], int n, char row, char col, char colour, char opcolour) {
     bool canBeFlipped = false;
     for (int i = -1; i < 2; i++) {
@@ -319,8 +334,8 @@ void getWinner(char board[][26], int n) {
             else if (board[i][j] == 'W') whiteNum++;
         }
     }
-    //printf("%d white\n", whiteNum);
-    //printf("%d black\n", blackNum);
+    // printf("%d white\n", whiteNum);
+    // printf("%d black\n", blackNum);
     if (blackNum > whiteNum) printf("B player wins.\n");
     else if (whiteNum > blackNum) printf("W player wins.\n");
     else printf("Draw!\n");
